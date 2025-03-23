9b36e326c27eeb3ef4857176c5bd779015617bc7
Abhishek <abhishek.raj292002@gmail.com>
2025-03-23 23:11:52 +0530

    Your message

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+let x = 10;
+console.log(x);
+/*function first(firstname : string){
+    console.log(`Hello ${firstname}`);
+}
+
+let s : string = "rahul";
+first(s);
+
+
+let a :number= 2;
+let b:number=5;
+function add(first:number,second:number) {
+    return first+second;
+}
+let res:number=add(a,b);
+console.log(res);*/
+function fun(age) {
+    return (age > 18);
+}
+let a = fun(18);
+console.log(a);
---
+++ index.ts
@@ -0,0 +1,24 @@
+let x:number =10;
+console.log(x)
+
+/*function first(firstname : string){
+    console.log(`Hello ${firstname}`);
+}
+
+let s : string = "rahul";
+first(s);
+
+
+let a :number= 2;
+let b:number=5;
+function add(first:number,second:number) {
+    return first+second;
+}
+let res:number=add(a,b);
+console.log(res);*/
+function fun(age:number){
+    return (age > 18)
+    
+}
+let a:boolean=fun(18);
+console.log(a);
